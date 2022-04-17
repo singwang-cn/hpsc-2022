89998a64c28e4dee9c639e651bfa1ef5ca919f41
WANG ZHAOQING <singw259@gmail.com>
2022-04-18 00:45:42 +0900

    13completed

--- 02_openmp/13_merge_sort.cpp
+++ 02_openmp/13_merge_sort.cpp
@@ -6,6 +6,7 @@ void merge(std::vector<int>& vec, int begin, int mid, int end) {
   std::vector<int> tmp(end-begin+1);
   int left = begin;
   int right = mid+1;
+
   for (int i=0; i<tmp.size(); i++) { 
     if (left > mid)
       tmp[i] = vec[right++];
@@ -16,6 +17,7 @@ void merge(std::vector<int>& vec, int begin, int mid, int end) {
     else
       tmp[i] = vec[right++]; 
   }
+
   for (int i=0; i<tmp.size(); i++) 
     vec[begin++] = tmp[i];
 }
@@ -23,8 +25,11 @@ void merge(std::vector<int>& vec, int begin, int mid, int end) {
 void merge_sort(std::vector<int>& vec, int begin, int end) {
   if(begin < end) {
     int mid = (begin + end) / 2;
+#pragma omp task shared(vec)
     merge_sort(vec, begin, mid);
+#pragma omp task shared(vec)
     merge_sort(vec, mid+1, end);
+#pragma omp taskwait
     merge(vec, begin, mid, end);
   }
 }
